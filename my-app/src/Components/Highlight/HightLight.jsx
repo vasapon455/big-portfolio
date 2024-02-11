0125e7a0fdbe299c1d998ba71dfa042d56e3216e
Vasapon Opaswatanakul <vasapon1996@gmail.com>
2024-02-11 21:51:53 +0700

    rearrange files

--- my-app/src/Components/Highlight/HightLight.jsx
+++
@@ -1,9 +0,0 @@
-import "./HighLight.css";
-
-export default function HighLight() {
-  return (
-    <section id="highlight">
-      <img src="assets/highlight.jpg" alt="profile" />
-    </section>
-  );
-}
