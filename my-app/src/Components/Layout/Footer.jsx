a5b97005d70767074097f0220222303fb68629cc
Vasapon Opaswatanakul <vasapon1996@gmail.com>
2024-02-12 21:58:06 +0700

    change folder name

--- my-app/src/Components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import Contact from "../Section/Contact/Contact";
-
-export default function Footer() {
-  return (
-    <footer>
-      <Contact />
-    </footer>
-  );
-}
--- my-app/src/Components/Section/OtherWorks/OtherWorks.jsx
+++
@@ -1,3 +0,0 @@
-export default function OtherWorks() {
-  return <section id="other-works"></section>;
-}
