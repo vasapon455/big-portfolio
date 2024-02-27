7861542dd646bebb7eaab8cf72c7e0e4d4ed7b4d
Vasapon Opaswatanakul <vasapon1996@gmail.com>
2024-02-27 09:38:47 +0700

    update component

--- BIG portfolio/src/Components/Section/OtherWorks/OtherWorks.jsx
+++ BIG portfolio/src/Components/Section/OtherWorks/OtherWorks.jsx
@@ -15,6 +15,6 @@ export default function OtherWorks() {
           <GridItem key={data.id} image={data.image} text={data.text} />
         ))}
       </GridContainer>
-    </section>
+    </section> 
   );
 }
